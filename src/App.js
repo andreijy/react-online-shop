cd2d90ed0608042d5c17d190f45a897b2308631d
Andrew Limaru <andrew.limaru@gmail.com>
2020-04-02 21:24:52 +1100

    add  react-router-dom; use withRouter on menu-item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component.jsx";
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
