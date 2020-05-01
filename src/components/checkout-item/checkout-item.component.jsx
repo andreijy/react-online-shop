61b476681409b861c918b8cbeed0b83c1e09b777
Andrew Limaru <3809608+andreijco@users.noreply.github.com>
2020-05-02 03:35:56 +1000

    add functionality to checkout-item component with increase item, decrease item and remove item. add the necessary actions to redux cart actions

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import {
+  clearItemFromCart,
+  removeItem,
+  addItem,
+} from "../../redux/cart/cart.actions";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, quantity, price } }) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img src={imageUrl} alt={name} />
+const CheckoutItem = ({ cartItem, dispatch }) => {
+  const { imageUrl, name, quantity, price } = cartItem;
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img src={imageUrl} alt={name} />
+      </div>
+      <span className='name'>{name}</span>
+      <span className='quantity'>
+        <div className='arrow' onClick={() => dispatch(removeItem(cartItem))}>
+          &#10094;
+        </div>
+        <span className='value'>{quantity}</span>
+        <div className='arrow' onClick={() => dispatch(addItem(cartItem))}>
+          &#10095;
+        </div>
+      </span>
+      <span className='price'>{price}</span>
+      <div
+        className='remove-button'
+        onClick={() => dispatch(clearItemFromCart(cartItem))}
+      >
+        &#10005;
+      </div>
     </div>
-    <span className='name'>{name}</span>
-    <span className='quantity'>{quantity}</span>
-    <span className='price'>{price}</span>
-    <div className='remove-button'>&#10005;</div>
-  </div>
-);
+  );
+};
 
-export default CheckoutItem;
+// const mapDispatchToProps = (dispatch) => ({
+//   clearItemFromCart: (cartItem) => dispatch(clearItemFromCart(cartItem)),
+// });
+
+export default connect(null)(CheckoutItem);
