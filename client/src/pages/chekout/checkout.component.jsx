b7f2c90a179d12905299d9246cf8157286bc935c
Andrew Limaru <3809608+andreijco@users.noreply.github.com>
2020-06-03 15:06:16 +1000

    create server.js using express to handle stripe backend payment and update the stripe button component

--- client/src/pages/chekout/checkout.component.jsx
+++ client/src/pages/chekout/checkout.component.jsx
@@ -38,7 +38,7 @@ const CheckoutPage = ({ cartItems, total }) => (
       <span>TOTAL: ${total}</span>
     </div>
     <div className='test-warning'>
-      *Please use the following test credit cart for payments*
+      *Please use the following test credit card for payments*
       <br />
       4242 4242 4242 4242 - Exp: 04/24 - CVC: 123
     </div>
