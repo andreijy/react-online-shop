4a1ad9506a3046c40271da5e9a9dc07ddc654103
Andrew Limaru <3809608+andreijco@users.noreply.github.com>
2020-06-04 16:32:43 +1000

    using the useContext way to use Context for collections/SHOP_DATA

--- src/components/collection-category/collection-category.component.jsx
+++ src/components/collection-category/collection-category.component.jsx
@@ -1,29 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import CollectionItem from "../collection-item/collection-item.component";
-import { selectCollection } from "../../redux/shop/shop.selectors";
 import CollectionsContext from "../../contexts/collections/collections.context";
 
 import "./collection-category.styles.scss";
 
-const CollectionCategory = ({ match }) => (
-  <CollectionsContext.Consumer>
-    {(collections) => {
-      const collection = collections[match.params.collectionId];
-      const { title, items } = collection;
-      return (
-        <div className='collection-category'>
-          <h2 className='title'>{title}</h2>
-          <div className='items'>
-            {items.map((item) => (
-              <CollectionItem key={item.id} item={item} />
-            ))}
-          </div>
-        </div>
-      );
-    }}
-  </CollectionsContext.Consumer>
-);
+const CollectionCategory = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
+  const { title, items } = collection;
+  return (
+    <div className='collection-category'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 // ownProps coming from Route built in passing parameters
 // const mapStateToProps = (state, ownProps) => ({
